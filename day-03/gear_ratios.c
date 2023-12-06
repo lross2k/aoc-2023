769f6df04da8c39b2faac2d13846cfff210f70ef
lross2k <luisross2000@outlook.com>
2023-12-05 23:18:15 -0600

    Day 3 completed, and started day 4

--- day-03/gear_ratios.c
+++ day-03/gear_ratios.c
@@ -4,6 +4,125 @@
 
 #define WORD_LEN 5
 
+void clear_word(char *word);
+
+int valid_char(char *word, char **data, int i, int j, int max_i, int max_j);
+long detect_numbers(char **rows, int index, int index2, int max_i, int max_j);
+int is_digit(char c);
+int find_digit_left(char **data, int j, int i, unsigned long long *subproduct);
+int find_digit_right(char **data, int j, int i, unsigned long long *subproduct);
+
+int main(void)
+{
+    FILE *fp;
+    char c;
+    char last_c;
+    int line = 0;
+    int column = 0;
+    int index;
+    int index2;
+    int word_index;
+    char **rows;
+    char *number_c;
+    int valid;
+    unsigned long long total = 0;
+    unsigned long long value = 0;
+
+    fp = fopen("input.txt", "r");
+    if (!fp) return(1);
+
+    while(1)
+    {
+        if (feof(fp)) break;
+        c = fgetc(fp);
+
+        if (c == '\n')
+            line++;
+        else if (line < 1)
+            column++;
+    }
+    fclose(fp);
+
+    /* Declare variables for text */
+    rows = (char**)malloc(line * (column * sizeof(char)));
+    for (index = 0; index < line; index++)
+        rows[index] = (char*)malloc(column * sizeof(char));
+
+    /* Initialize variables */
+    for (index2 = 0; index2 < line; index2++)
+        for (index = 0; index < column; index++)
+            rows[index2][index] = '\0';
+
+    fp = fopen("input.txt", "r");
+    if (!fp) return(1);
+    for (index2 = 0; index2 < line; index2++)
+    {
+        for (index = 0; index < column; index++)
+        {
+            if (feof(fp)) break;
+            c = fgetc(fp);
+            if (c == '\n')
+                c = fgetc(fp);
+            rows[index2][index] = c > 10 ? c : ' ';
+        }
+    }
+    fclose(fp);
+
+    number_c = (char *)malloc(WORD_LEN * sizeof(char));
+    clear_word(number_c);
+
+    for (index2 = 0; index2 < line; index2++)
+    {
+        for (index = 0; index < column; index++)
+        {
+            last_c = c;
+            c = rows[index2][index];
+            if (is_digit(c))
+            {
+                number_c[word_index] = c;
+                word_index++;
+            }
+            else if (c <= '/' && last_c > '/')
+            {
+                valid = valid_char(number_c, rows, index, index2, line, column);
+                if (valid)
+                {
+                    value = valid ? strtol(number_c, NULL, 0) : 0;
+                    total += value;
+                }
+                word_index = 0;
+                clear_word(number_c);
+            }
+        }
+    }
+
+    printf("First part value: %lli\n", total);
+
+    total = 0;
+    value = 0;
+
+    for (index2 = 0; index2 < line; index2++)
+    /*
+    for (index2 = 35; index2 < 36; index2++)
+    */
+    {
+        for (index = 0; index < column; index++)
+        {
+            last_c = c;
+            c = rows[index2][index];
+            if (c == '*')
+            {
+                value = detect_numbers(rows, index, index2, line, column);
+                total += value; 
+            }
+        }
+    }
+
+    printf("Second part value: %lli\n", total);
+
+    return(0);
+}
+
 void clear_word(char *word)
 {
     int index;
@@ -36,123 +155,129 @@ int valid_char(char *word, char **data, int i, int j, int max_i, int max_j)
     /* right */
     right = i < max_i ? i : i - 2;
 
-    /*
-    printf("\nword: %s top: %i bottom: %i left: %i right: %i\n", word, top, bottom, left, right);
-    */
-
+    for (index_j = top; index_j <= bottom; index_j++)
     for (index_j = top; index_j <= bottom; index_j++)
         for (index_i = left; index_i <= right; index_i++)
         {
             c = data[index_j][index_i];
             if ((c <= '/' && c >= '!' && c != '.') || (c >= ':' && c <= '@'))
-            {
-                printf(" %c ", c);
                 return(1);
-            }
         }
     return(0);
 }
 
-int main(void)
+int is_digit(char c)
 {
-    FILE *fp;
-    char c;
-    char last_c;
-    int line = 0;
-    int column = 0;
-    int index;
-    int index2;
-    int word_index;
-    char **rows;
-    char *number_c;
-    int valid;
-    long int total = 0;
-    long int value;
+    return(c >= '0' && c <= '9');
+}
 
-    fp = fopen("input.txt", "r");
-    if (!fp) return(1);
+int find_digit_left(char **data, int j, int i, unsigned long long *subproduct)
+{
+    int index_i;
+    int end_index_i = -1;
 
-    while(1)
+    for (index_i = i; ; index_i--)
     {
-        if (feof(fp)) break;
-        c = fgetc(fp);
-
-        if (c == '\n')
-            line++;
-        else if (line < 1)
-            column++;
-    }
-    fclose(fp);
-    printf("lines: %i, columns %i\n", line, column);
-
-    /* Declare variables for text */
-    rows = (char**)malloc(line * (column * sizeof(char)));
-    for (index = 0; index < line; index++)
-        rows[index] = (char*)malloc(column * sizeof(char));
-
-    /* Initialize variables */
-    for (index2 = 0; index2 < line; index2++)
-        for (index = 0; index < column; index++)
-            rows[index2][index] = '\0';
-
-    fp = fopen("input.txt", "r");
-    if (!fp) return(1);
-    for (index2 = 0; index2 < line; index2++)
-    {
-        for (index = 0; index < column; index++)
+        if(!is_digit(data[j][index_i]))
         {
-            if (feof(fp)) break;
-            c = fgetc(fp);
-            if (c == '\n')
-                c = fgetc(fp);
-            rows[index2][index] = c > 10 ? c : ' ';
+            end_index_i = find_digit_right(data, j, index_i + 1, subproduct);
+            break;
         }
     }
-    fclose(fp);
+    return(end_index_i);
+}
 
-    /*
-    for (index2 = 0; index2 < line; index2++)
+int find_digit_right(char **data, int j, int i, unsigned long long *subproduct)
+{
+    int index_i, sub_index_i;
+    int end_index_i = -1;
+    char c_number[6] = {'\0'};
+    unsigned long long value = 0;
+
+    for (index_i = i; ; index_i++)
     {
-        for (index = 0; index < column; index++)
-            printf("%c", rows[index2][index]);
-        printf("\n");
-    }
-    */
-
-    number_c = (char *)malloc(WORD_LEN * sizeof(char));
-    clear_word(number_c);
-
-    for (index2 = 0; index2 < line; index2++)
-    {
-        for (index = 0; index < column; index++)
+        if(!is_digit(data[j][index_i]))
         {
-            last_c = c;
-            c = rows[index2][index];
-            if (c >= '0' && c <= '9')
+            end_index_i = index_i - 1;
+            for (sub_index_i = i; sub_index_i <= end_index_i; sub_index_i++)
+                c_number[sub_index_i - i] = data[j][sub_index_i];
+            value = strtol(c_number, NULL, 0);
+            *subproduct *= value;
+            clear_word(c_number);
+            break;
+        }
+    }
+
+    return(end_index_i);
+}
+
+long detect_numbers(char **data, int i, int j, int max_i, int max_j)
+{
+    int top, left, right, bottom;
+    int index_i, index_j;
+    int end_index_i;
+    char c;
+    char single[2] = {'\0'};
+    unsigned long long subproduct = 1;
+    int part_numbers = 0;
+
+    /* top */
+    top = j > 0 ? j - 1 : j;
+    /* left */
+    left = i > 0 ? i - 1 : i;
+    /* bottom */
+    bottom = j < max_j ? j + 1 : j;
+    /* right */
+    right = i < max_i ? i + 1 : i;
+
+    for (index_j = top; index_j <= bottom; index_j++)
+        for (index_i = left; index_i <= right; index_i++)
+        {
+            c = data[index_j][index_i];
+            if (is_digit(c))
             {
-                number_c[word_index] = c;
-                word_index++;
-            }
-            else if (c <= '/' && last_c > '/')
-            {
-                valid = valid_char(number_c, rows, index, index2, line, column);
-                if (valid)
+                if (is_digit(data[index_j][index_i - 1]))
                 {
-                    value = valid ? strtol(number_c, NULL, 0) : 0;
-                    total += value;
+                    /* digit continues to the left, find where it ends */
+                    end_index_i = find_digit_left(data, index_j, index_i, &subproduct);
+                    part_numbers++;
+                }
+                else if (is_digit(data[index_j][index_i + 1]))
+                {
+                    /* digit continues to the right, find where it ends */
+                    end_index_i = find_digit_right(data, index_j, index_i, &subproduct);
+                    part_numbers++;
                 }
                 else
-                    printf("%s %li %i\n", number_c, strlen(number_c), valid);
-                word_index = 0;
-                clear_word(number_c);
+                {
+                    /* single character number */
+                    end_index_i = index_i;
+                    single[0] = c;
+                    subproduct *= strtol(single, NULL, 0);
+                    single[0] = '\0';
+                    part_numbers++;
+                }
+
+                if (end_index_i > index_i && end_index_i >= right && index_j < bottom)
+                {
+                    index_i = left - 1;
+                    index_j++;
+                    end_index_i = -1;
+                }
+                else if (end_index_i > index_i && end_index_i < right && index_j < bottom)
+                {
+                    index_i = end_index_i;
+                    end_index_i = -1;
+                }
+                else if (index_j == bottom && index_i < right && end_index_i < right && end_index_i > index_i)
+                    index_i = end_index_i + 1;
+                else if (end_index_i == right && index_j == bottom)
+                    index_i = end_index_i;
+                else if (end_index_i > right && index_j == bottom)
+                    break;
             }
         }
-    }
 
-    printf("Total value: %li\n", total);
-
-    return(0);
+    return(part_numbers == 2 ? subproduct : 0);
 }
-
-
 
---
+++ day-04/scratchcard_checking.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *fp;
+    char c;
+
+}
+

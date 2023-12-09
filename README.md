c0c62a563063ed86392a4bcd98f9074d0d00e37c
lross2k <luisross2000@outlook.com>
2023-12-08 18:27:41 -0600

    Day 06 finished

---
+++ README.md
@@ -0,0 +1,18 @@
+# Advent of Code 2023
+
+The goal is to finish my first aoc, for which I will be using ANSI C (C89) to learn a bit more of C before learning C11/17 or C2X
+
+# Done
+
+- day-01
+- day-02
+- day-03
+- day-04
+- day-05 (second part missing)
+- day-06
+
+# Excuses / TODO
+
+- I won't try to keep up with the daily release since I'm currently busy
+- This repository is currently a mess, I will clean it and the code solutions before archiving
+
--- day-06/racing.c
+++ day-06/racing.c
@@ -1,83 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define FILE_LINES 220
 #define LINE_WORDS 25
 #define WORD_CHARS 25
 char *** load_data(int *lines, char *filename);
 int is_empty(char *word);
+void print_words(char **line);
 long * get_seeds(char ***data);
 long get_param(char ***data, int lines, long compare_value, char *first_identifier, char *second_identifier);
 long get_location(char ***data, int doc_lines, long seed);
+void print_sentences(char ***data, long line_len);
+
+int is_word(char *word)
+{
+    int result;
+    result = word[0] != '\0' && word[0] != '\n' && word[0] != ' ';
+    return(result);
+}
 
 int main(void)
 {
     char ***data;
-    long *seeds;
-    long *locations;
-    long new_location;
     int doc_lines;
-    long i, j;
-    long min_location_seed, min_location;
+    long time_index, distance_index;
+    long current_record_distance, current_max_time;
+    double x_1, x_2, discriminant, top_value, bottom_value;
+    long i;
+    int total;
 
-    data = load_data(&doc_lines, "input.txt");
+    data = load_data(&doc_lines, "input2.txt");
     if (!data)
     {
         printf("NO DATA\n");
         return(1);
     }
 
-    locations = (long *)malloc(LINE_WORDS * sizeof(long));
-    for (i = 0; i < LINE_WORDS; i++)
-        locations[i] = -1;
+    print_sentences(data, LINE_WORDS);
 
-    seeds = get_seeds(data);
-    for (i = 0; i < LINE_WORDS; i++)
-        if (seeds[i] > 0)
-        {
-            printf("SEED %li\n", seeds[i]);
-            locations[i] = get_location(data, doc_lines, seeds[i]);
-        }
-        else
-            break;
-    
-    min_location = 0;
-    for (i = 0; i < LINE_WORDS; i++)
-        if (seeds[i] > 0)
-        {
-            if (locations[i] < locations[min_location] && locations[min_location] > 0)
-                min_location = i;
-        }
-        else
-            break;
-    
-    printf("First part minimum location: %li of seed: %li\n", locations[min_location], seeds[min_location]);
-
-    /* Second part */
-    min_location_seed = 0;
-    min_location = get_location(data, doc_lines, seeds[0]);
-    printf("START seed %li location %li\n", seeds[0], min_location);
-    for (i = 0; i < LINE_WORDS; i = i + 2)
+    for (i = 0; i < FILE_LINES; i++)
     {
-        if (seeds[i] > 0)
-        {
-            for (j = 0; j < seeds[i + 1]; j++)
-            {
-                new_location = get_location(data, doc_lines, seeds[i] + j);
-                if (new_location < min_location && new_location != 0)
-                {
-                    printf("NEW LOWEST LOCATION %li < %li\n", new_location, min_location);
-                    min_location = new_location;
-                    min_location_seed = seeds[i] + j;
-                }
-            }
-        }
-        else
+        if (!is_word(data[i][0]))
             break;
+        else if(strcmp(data[i][0], "Time") == 0)
+            time_index = i;   
+        else if(strcmp(data[i][0], "Distance") == 0)
+            distance_index = i;   
     }
 
-    printf("Second part minimum location: %li of seed: %li\n", min_location, min_location_seed);
+    total = 1;
+    for (i = 1; i < LINE_WORDS; i++)
+    {
+        if (!is_word(data[time_index][i]))
+            break;
+
+/* Distance line has the record distance in millimeters */
+        current_record_distance = strtol(data[distance_index][i], NULL, 0);
+/* Time line has the maximum duration in milliseconds */
+        current_max_time = strtol(data[time_index][i], NULL, 0);
+
+/* Boat speed starts at 0 millimeters / millisecond */
+/* Boat speed increases 1 mm / ms for each whole ms held at the beginning */
+        /* 200 mm on 30 ms 
+         * means min_beating_speed = 200 mm / 30 ms 
+         * don't even try holding_time = 0 or holding_time = t_n
+         * distance = t_max*t - t^2
+         * t^2 - t_max*t + distance = 0 
+         * a = 1 
+         * b = -t_max 
+         * c = distance */
+        discriminant = current_max_time * current_max_time - 4 * current_record_distance;
+        x_1 = (-1 * current_max_time + sqrt(discriminant))/(2 * -1);
+        x_2 = (-1 * current_max_time - sqrt(discriminant))/(2 * -1);
+        
+        bottom_value = ceil(x_1) > x_1 ? ceil(x_1) : x_1 + 1.0;
+        top_value = floor(x_2) < x_2 ? floor(x_2) : x_2 - 1.0;
+        
+        total *= (int)(top_value - bottom_value + 1.0);
+    }
+
+    printf("Total value: %i\n", total);
 
     return(0);
 }
@@ -125,21 +129,17 @@ char *** load_data(int *lines, char *filename)
 
         if (c == '\n')        
         {
-            /* New line chars */
-            /*
-            print_line(data[line]);
-            */
             line++;
             word = 0;
             ch = 0;
         }
-        else if (c == ' ' || c == '-')
+        else if ((c == ' ' || c == '-') && ch > 0)
         {
             /* New word chars  */
             word++;
             ch = 0;
         }
-        else if (c != ':')
+        else if (c != ':' && c != ' ')
         {
             /* Add char except ignore chars */
             data[line][word][ch] = c; 
@@ -224,5 +224,27 @@ long get_param(char ***data, int lines, long compare_value, char *first_identifi
         }
     }
     return(compare_value);
+}
+
+void print_words(char **line)
+{
+    int i;
+    for (i = 0; i < LINE_WORDS; i++)
+        if (line[i][0] != '\0' && line[i+1][0] != '\0')
+            printf("%s,", line[i]);
+        else if (line[i][0] != '\0')
+            printf("%s\n", line[i]);
+        else
+            break;
+}
+
+void print_sentences(char ***data, long line_len)
+{
+    long i;
+    for (i = 0; i < line_len; i++)
+        if (data[i][0][0] != '\0')
+            print_words(data[i]);
+        else
+            break;
 }
 
